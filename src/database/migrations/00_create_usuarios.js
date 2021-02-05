75ff0e426843df4127b388483ab08377fdd69100
MatheusAnciloto <ancilotom@gmail.com>
2021-02-05 19:47:42 -0400

    criada migration para tabela de usuários

---
+++ src/database/migrations/00_create_usuarios.js
@@ -0,0 +1,15 @@
+const knex = require('knex');
+
+exports.up = function(knex){
+    return knex.schema.createTable('usuarios', table => {
+        table.increments('id').primary();
+        table.string('nome').notNullable();
+        table.string('email').notNullable();
+        table.string('senha').notNullable();
+    })
+};
+
+
+exports.down = function(knex){
+    return knex.schema.dropTable('usuarios');
+};
